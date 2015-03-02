98f010f1082e5a08bf1748058ac07e25c1c64790
raichu1 <yc3de@virginia.edu>
2015-03-02 16:13:54 -0500

    "conflict test owen"

--- manage.py
+++ manage.py
@@ -8,7 +8,5 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "untitled1.settings")
 
     from django.core.management import execute_from_command_line
-
+#chickenseatdogs
     execute_from_command_line(sys.argv)
-
-#bigbugs
