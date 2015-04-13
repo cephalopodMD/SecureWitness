8c8a229c4b8f2c29e99700be950a61bd13ab1e83
rademuth <rad9aw@virginia.edu>
2015-04-13 16:15:17 -0400

    added database tracking

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -9,15 +9,15 @@ import app.models
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('auth', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Attachment',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('file', models.FileField(upload_to=app.models.get_upload_path)),
                 ('encrypted', models.BooleanField(default=False)),
             ],
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Folder',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(help_text='Folder Name', max_length=128)),
                 ('slug', models.SlugField()),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
@@ -40,16 +40,16 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Report',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('timeCreated', models.DateTimeField(verbose_name='Time created')),
                 ('shortDesc', models.CharField(help_text='Short description', max_length=128)),
                 ('detailedDesc', models.TextField(help_text='Detailed description')),
-                ('location', models.CharField(help_text='Location (optional)', max_length=128, blank=True)),
-                ('dateOfIncident', models.DateField(help_text='Date of incident (optional)', blank=True, null=True)),
-                ('keywords', models.CharField(help_text='Keywords (optional)', max_length=128, blank=True)),
+                ('location', models.CharField(help_text='Location (optional)', blank=True, max_length=128)),
+                ('dateOfIncident', models.DateField(null=True, blank=True, help_text='Date of incident (optional)')),
+                ('keywords', models.CharField(help_text='Keywords (optional)', blank=True, max_length=128)),
                 ('private', models.BooleanField(default=False, help_text='Private')),
-                ('folder', models.ForeignKey(default=None, null=True, to='app.Folder', blank=True)),
-                ('groups', models.ManyToManyField(to='auth.Group', default=None, blank=True, null=True)),
+                ('folder', models.ForeignKey(default=None, blank=True, to='app.Folder', null=True)),
+                ('groups', models.ManyToManyField(null=True, default=None, blank=True, to='auth.Group')),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
