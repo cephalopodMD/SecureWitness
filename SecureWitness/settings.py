47bdade0d55b38f18db8135b8a5fe7462aae2932
cephalopodMD <mistahowe@gmail.com>
2015-04-20 15:50:52 -0400

    settings.py stuff

--- SecureWitness/settings.py
+++ SecureWitness/settings.py
@@ -56,9 +56,14 @@ WSGI_APPLICATION = 'SecureWitness.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-import dj_database_url
-DATABASES = {}
-DATABASES['default'] =  dj_database_url.config()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
